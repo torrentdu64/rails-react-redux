d51693114feb133332990594ea78ca238ddaca1e
torrent benoit <torrentdu64@gmail.com>
2019-07-28 17:24:38 +1200

    add controller for user

---
+++ app/controllers/registrations_profiles_controller.rb
@@ -0,0 +1,21 @@
+class RegistrationsProfilesController < Devise::RegistrationsController
+
+  def sign_up_params
+    devise_parameter_sanitizer.sanitize(:sign_up)
+  end
+
+  def account_update_params
+    devise_parameter_sanitizer.sanitize(:account_update)
+  end
+
+  private
+
+  def sign_up_params
+    params.require(:user).permit(:email, :phone, :password, :password_confirmation, :profile)
+  end
+
+  def account_update_params
+    params.require(:user).permit(:email, :phone, :password, :password_confirmation)
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
+  devise_for :users, :controllers => { registrations: 'registrations_profile' }
 
-  devise_for :users
   root to: 'profiles#list'
 
 
