eb6e4c65b899103a940567a548665446942b8d5b
torrent benoit <torrentdu64@gmail.com>
2019-08-05 10:39:23 +1200

    add views reply no test return 500

--- app/controllers/api/v1/bookings_controller.rb
+++ app/controllers/api/v1/bookings_controller.rb
@@ -24,8 +24,8 @@ class Api::V1::BookingsController < Api::V1::BaseController
       #record message_id to booking
       @message_id = res["message_id"]
 
-      @booking = @booking.update(message_id: @message_id)
-      binding.pry
+      @booking = @booking.update_columns(message_id: @message_id)
+
       p "====================================="
 
       render :create, status: :created
@@ -35,22 +35,37 @@ class Api::V1::BookingsController < Api::V1::BaseController
   end
 
   def reply
+
       # in-put => message_id
-      # @booking = booking.find_by(message_id: message_id)
-      # @user = User.fin_by(id: @booking.user_id )
-      # if yes
+      p message_id = params["message_id"]
+      p response = params["response"]
+      # make one request to db refacto
+      @booking = Booking.find_by(message_id: message_id)
+      @user = User.find_by(id: @booking.user_id )
+      @profile = Profile.find_by(id: @booking.profile_id )
+
+      affirmation = ["yes" , "Yes", "ok", "Ok", "y", "Y", "YES", "接收", "接 收", "接受" , "接 受"]
+      negation = ["no" , "No", "N", "NO", "n", "na", "Na", "NA", "不接收", "不接受" ]
+
+      confirmed = affirmation.select{|x| x.match(response) }.length > 0
+      declined = negation.select{|x| x.match(response) }.length > 0
+
+       if confirmed
       #   payment.charge
       #   send sms info and charge
-      # else no
-      #   send info no charge
-      # end
-      #
-      #
-      # reply correct user
-     @sms = SmsApi.new(ENV['BURST_API_KEY'], ENV['BURST_API_SECRET'])
-     message = " ping pong hot dog"  #is your verification code.
-     response = @sms.send(message, "+642041845759" )
+        @sms = SmsApi.new(ENV['BURST_API_KEY'], ENV['BURST_API_SECRET'])
+        message = "Your Booking is confirm at #{@booking.start_time} with #{@profile.name}"  #is your verification code.
+        response = @sms.send(message, "+642041845759" )
 
+       elsif negation
+      #   send info no charge
+        @sms = SmsApi.new(ENV['BURST_API_KEY'], ENV['BURST_API_SECRET'])
+        message = "Your Booking is declined with #{@profile.name}"  #is your verification code.
+        response = @sms.send(message, "+642041845759" )
+
+       end
+
+      render :reply, status: :created
   end
 
   private
