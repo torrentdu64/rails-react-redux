24edda0e620bb61753713cdba3bf38661c256f10
torrent benoit <torrentdu64@gmail.com>
2019-08-15 14:03:17 +1200

    delete api booking controller fetchbusytime

--- app/controllers/api/v1/bookings_controller.rb
+++ app/controllers/api/v1/bookings_controller.rb
@@ -29,7 +29,6 @@ class Api::V1::BookingsController < Api::V1::BaseController
   end
 
   def booking_time
-    # receive the selected date
 
      selected_date = params[:q]
       #binding.pry
@@ -38,28 +37,9 @@ class Api::V1::BookingsController < Api::V1::BaseController
       res =  selected_date.to_date.to_formatted_s(:rfc822)
 
       end_point = res.to_date + 1.day
-     # binding.pry
+
     @booking = Booking.where(profile_id: params[:profile_id]).where("start_time >= ?  AND end_time <= ?", res, end_point )
-      #binding.pry
-    # time_start_array = []
-    # time_duration_array = []
-    # time_end_array = []
-    #binding.pry
-    # @booking.each do |time|
-    #   time_start_array << time.start_time
-    #   time_duration_array << time.duration
-    #   time_end_array << time.end_time
-    # end
 
-    # time_start_array.each_with_index do |time, i|
-    #    p time_duration_array[i]
-
-    # end
-
-    # binding.pry
-    #binding.pry
-    #create a array with each 30 min between start and end booking
-    #and all from 00:00 to current time if current date
      authorize @booking
 
     render :booking_time, status: 200
