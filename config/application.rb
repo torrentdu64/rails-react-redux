49988aa1d6da40d3792ec9e8b467a68bf3ed225a
torrent benoit <torrentdu64@gmail.com>
2019-08-18 11:05:44 +1200

    Pacific/Auckland to config application

--- config/application.rb
+++ config/application.rb
@@ -15,8 +15,9 @@ module RailsReactRedux
         end
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
-    config.time_zone = 'Auckland'
+    config.time_zone = 'Pacific/Auckland'
     config.active_record.default_timezone = :local
+    #config.active_record.time_zone_aware_attributes = false
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
