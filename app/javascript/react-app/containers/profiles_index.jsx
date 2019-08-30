ea408630834f5b68a42409c7266940bc75d234a1
torrent benoit <torrentdu64@gmail.com>
2019-08-30 13:08:35 +1200

    listing profile html css

--- app/javascript/react-app/containers/profiles_index.jsx
+++ app/javascript/react-app/containers/profiles_index.jsx
@@ -62,40 +62,29 @@ class ProfilesIndex extends Component {
 
     return this.props.profiles.map((profile) => {
       return (
-        <div className="card card-cascade" style={{display: 'inline-block', width: 'calc(100% * (1/5) - 10px - 1px)', margin: '10px 0 0 2%', flexGrow: '1' }}>
-
-
-  <div className="view view-cascade overlay">
-    <img className="card-img-top" src="https://mdbootstrap.com/img/Photos/Others/men.jpg" alt="Card image cap" />
-    <a>
-      <div className="mask rgba-white-slight"></div>
-    </a>
-  </div>
-
-
-  <div className="card-body card-body-cascade text-center">
-
-      <h4 className="card-title"><strong>Billy Coleman</strong></h4>
-
-    <h6 className="font-weight-bold indigo-text py-2">Web developer</h6>
-     <p className="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus, ex, recusandae. Facere modi sunt, quod quibusdam.
-    </p>
-
-
-    <a type="button" className="btn-floating btn-small btn-fb"><i className="fab fa-facebook-f"></i></a>
-        <a type="button" className="btn-floating btn-small btn-tw"><i className="fab fa-twitter"></i></a>
-
-    <a type="button" className="btn-floating btn-small btn-dribbble"><i className="fab fa-dribbble"></i></a>
-
-  </div>
-
-
-  <div className="card-footer text-muted text-center">
-    2 days ago
-  </div>
-
-</div>
-
+        <div className="card card-cascade"
+             style={{display: 'inline-block', width: 'calc(100% * (1/5) - 10px - 1px)', margin: '10px 0 0 2%', flexGrow: '1' }}
+             key={profile.id}
+        >
+          <div className="view view-cascade overlay">
+            <img className="card-img-top" src="https://mdbootstrap.com/img/Photos/Others/men.jpg" alt="Card image cap" />
+            <a>
+              <div className="mask rgba-white-slight"></div>
+            </a>
+          </div>
+          <div className="card-body card-body-cascade text-center">
+              <h4 className="card-title"><strong>{profile.name}</strong></h4>
+            <h6 className="font-weight-bold indigo-text py-2">Web developer</h6>
+             <p className="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus, ex, recusandae. Facere modi sunt, quod quibusdam.
+            </p>
+            <a href={`/profiles/${profile.id}`} >
+              <button type="button" className="btn strip-button">Profile</button>
+            </a>
+          </div>
+          <div className="card-footer text-muted text-center">
+            2 days ago
+          </div>
+        </div>
         );
     });
   }
