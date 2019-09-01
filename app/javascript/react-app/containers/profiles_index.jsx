027231021fca0913dd999c0b0e1d24559812a29a
torrent benoit <torrentdu64@gmail.com>
2019-09-02 11:02:23 +1200

    dynamic name and link to profile show

--- app/javascript/react-app/containers/profiles_index.jsx
+++ app/javascript/react-app/containers/profiles_index.jsx
@@ -75,18 +75,14 @@ class ProfilesIndex extends Component {
 
   <div className="card-body card-body-cascade text-center">
 
-      <h4 className="card-title"><strong>Billy Coleman</strong></h4>
+      <h4 className="card-title"><strong>{profile.name}</strong></h4>
 
     <h6 className="font-weight-bold indigo-text py-2">Web developer</h6>
      <p className="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus, ex, recusandae. Facere modi sunt, quod quibusdam.
     </p>
 
 
-    <a type="button" className="btn-floating btn-small btn-fb"><i className="fab fa-facebook-f"></i></a>
-        <a type="button" className="btn-floating btn-small btn-tw"><i className="fab fa-twitter"></i></a>
-
-    <a type="button" className="btn-floating btn-small btn-dribbble"><i className="fab fa-dribbble"></i></a>
-
+    <a href={`profiles/${profile.id}`} className="btn btn-primary">Profile</a>
   </div>
 
 
