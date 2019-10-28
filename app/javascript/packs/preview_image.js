22460381a04db85c88cd7ce7ad3aa4265ae98df3
torrent benoit <torrentdu64@gmail.com>
2019-10-28 16:44:55 +1300

    upload image and display img background in head of page

---
+++ app/javascript/packs/preview_image.js
@@ -0,0 +1,24 @@
+function readURL() {
+
+  alert("hello");
+
+// if (input.files && input.files[0]) {
+//           var reader = new FileReader();
+
+//           reader.onload = (e) => {
+
+
+
+//                $('#img_prev_photoone')
+//                 .attr('src', e.target.result)
+//                 .width(150)
+//                 .height(200);
+
+
+
+
+//           };
+
+//           reader.readAsDataURL(input.files[0]);
+//         }
+}
